d7837ad19968ab46156fcbc277cd17917852c5c4
Manjot-git <manjott13k@gmail.com>
2025-07-01 12:48:11 +0530

    All fixed

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use((req,res,next) =>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user || null;
-    console.log("Current User in middleware:", req.user);
+    // console.log("Current User in middleware:", req.user);
 
     next();
 });
