68ad611a77c1e5bd8541ebac7527e7e09c400478
Manjot-git <manjott13k@gmail.com>
2025-07-01 12:30:31 +0530

    navbar fixed

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store =  MongoStore.create({
     mongoUrl: dbUrl, //in atlasDB stores session info
     crypto:{
-        secret: process.env.secret,
+        secret: process.env.SECRET,
     },
     touchAfter: 24 * 3600,
 });     
